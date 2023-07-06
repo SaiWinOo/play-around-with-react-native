09e2e602ee76f16f3f71505b91a85bf52c2ea2b2
saiwinoo <saiwinoo52@gmail.com>
2023-07-06 19:27:16 +0630

    install nativewind

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text className="text-red-400">fsdfa</Text>
+    </View>
+  );
+};
+
+export default App;
